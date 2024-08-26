d2e4617c850f3d5c7d36fcb5dba3c43bfa2919ff
Sander Fossedal <sander.fossedal@gmail.com>
2024-08-26 17:10:06 +0200

    Initial commit, frontend finished

---
+++ api/Models/Comment.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime createdOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; }
+        //navigation property () .net core will automatically create a foreign key in the database
+
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ api/Models/Stock.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18, 2)")] //Forces the database to use decimal(18, 2) for this property
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")] //Forces the database to use decimal(18, 2) for this property
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
